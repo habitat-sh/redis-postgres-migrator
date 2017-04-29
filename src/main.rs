b460a12d59bca5abc62d9395216fbb8b91e3e5b7
Nell Shamrell <nellshamrell@gmail.com>
2017-04-28 21:30:32 -0700

    working test harness

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,18 @@
 extern crate redis_postgres_migrator;
 
 fn main() {
+// redis_postgres_migrator::builder-sessionsrv::data_store::
     println!("bite me");
+}
+
+pub fn redis_to_postgres(thing: String) {
+    println!("{}", thing);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    fn test_data_transfer() {
+        redis_to_postgres("Bite me!".to_string())
+    }
 }
