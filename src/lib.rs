fde1498d14957c91162dc82fd596a1c8378f0361
Nell Shamrell <nellshamrell@gmail.com>
2017-04-28 21:08:56 -0700

    wip

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,7 @@ extern crate habitat_builder_dbcache as dbcache;
 extern crate habitat_builder_protocol as protocol;
 extern crate habitat_core as hab_core;
 extern crate habitat_net as hab_net;
+extern crate habitat_builder_sessionsrv as hab_sessionsrv;
 extern crate hyper;
 extern crate log;
 extern crate protobuf;
--- src/redis_data_store/mod.rs
+++ src/redis_data_store/mod.rs
@@ -4,5 +4,6 @@ use std::sync::Arc;
 use dbcache;
 use dbcache::data_store::*;
 use hab_net;
+use hab_sessionsrv;
 use protocol::sessionsrv;
 use redis::{self, Commands, PipelineCommands};
