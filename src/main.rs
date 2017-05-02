319836bdb03b37a513a68bc6d28ada4e0ff49ead
Nell Shamrell <nellshamrell@gmail.com>
2017-05-01 17:20:41 -0700

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -51,9 +51,6 @@ mod tests {
 
 #[test]
 fn create_account() {
-
-//    let config = session_srv_config::Config::default();
-
     let mut sc = proto_session::SessionCreate::new();
     sc.set_token(String::from("hail2theking"));
     sc.set_extern_id(64);
@@ -62,48 +59,15 @@ fn create_account() {
     sc.set_provider(proto_session::OAuthProvider::GitHub);
 
     let config = Default::default();
-
     let manager = RedisConnectionManager::new("redis://localhost").unwrap();
     let pool = Arc::new(r2d2::Pool::new(config, manager).unwrap());
 
 		let pool1 = pool.clone();
-		let pool2 = pool.clone();
-		let pool3 = pool.clone();
 
-//		let ds = sessionsrv_data_store::DataStore {
-//             pool: pool,
-//             accounts: sessionsrv_data_store::AccountTable::new(pool1),
-//             features: sessionsrv_data_store::FeatureFlagsIndices::new(pool2),
-//             sessions: sessionsrv_data_store::SessionTable::new(pool3)};
+		let account_table = sessionsrv_data_store::AccountTable::new(pool1);
+		let account = sessionsrv_data_store::AccountTable::find_or_create(&account_table, &sc);
 
-//    let session = ds.find_or_create_account_via_session(&sc, true, false, false)
-//        .expect("Should create account");
-
-
-//    let mut account = proto_session::Account::new();
-//    account.set_email(sc.get_email().to_string());
-//    account.set_name(sc.get_name().to_string());
-//    sessionsrv_data_store.write(&mut account);
-//    hab_sessionsrv::data_store::DataStore.write(&mut account);
-
-//    DataStore.session_create(&sc);
-//let ds = DataStore::from_pool(pool).expect("Failed to create data store from pool");
-
- //   let session = ds.find_or_create_account_via_session(&sc, true, false, false)
- //       .expect("Should create account");
- //   assert!(session.get_id() != 0, "Created account has an ID");
- //   assert_eq!(session.get_email(), "bobo@chef.io");
- //   assert_eq!(session.get_name(), "Bobo T. Clown");
-
-  //  let session2 = ds.find_or_create_account_via_session(&sc, true, false, false)
-  //      .expect("Should return account");
- //   assert_eq!(session.get_id(), session2.get_id());
-  //  assert_eq!(session.get_email(), session2.get_email());
-   // assert_eq!(session.get_name(), session2.get_name());
-
-//let session = sessionsrv_data_store::SessionTable::new(pool1);
-let account_table = sessionsrv_data_store::AccountTable::new(pool1);
-let account = sessionsrv_data_store::AccountTable::find_or_create(&account_table, &sc);
+    println!("{:?}", account);
 }
 
 
