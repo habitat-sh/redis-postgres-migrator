3dfa68b32a952882eac9b32893575c9fdc7e6e76
Nell Shamrell <nellshamrell@gmail.com>
2017-05-02 17:05:47 -0700

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 extern crate redis_postgres_migrator_redis_lib;
+extern crate redis_postgres_migrator_postgres_lib;
+
 use redis_postgres_migrator_redis_lib as redis_lib;
+use redis_postgres_migrator_postgres_lib as postgres_lib;
 
 fn main() {
     println!("bite me");
@@ -30,14 +33,13 @@ mod tests {
 
     #[test]
     fn test_postgres_account_create() {
-//        let session = postgres_data_store::create_session(
-//            String::from("hail2theking"),
-//            64,
-//            String::from("bobo@chef.io"),
-//            String::from("Bobo T. Clown"),
-//        );
+        let session = postgres_lib::create_session(
+            String::from("hail2theking"),
+            64,
+            String::from("bobo@chef.io"),
+            String::from("Bobo T. Clown"),
+        );
 
-        //let account = redis_postgres_migrator::postgres_data_store::create_account()
-
+        let account = postgres_lib::create_account(session);
     }
 }
--- src/redis_postgres_migrator_postgres_lib/src/lib.rs
+++ src/redis_postgres_migrator_postgres_lib/src/lib.rs
@@ -2,4 +2,22 @@ extern crate habitat_builder_sessionsrv as hab_sessionsrv;
 extern crate habitat_builder_protocol as protocol;
 extern crate habitat_core as hab_core;
 extern crate habitat_net as hab_net;
+extern crate habitat_builder_db as hab_db;
 
+pub fn create_session(token: String, extern_id: u64, email: String, name: String) -> protocol::sessionsrv::SessionCreate {
+    let mut sc = protocol::sessionsrv::SessionCreate::new();
+    sc.set_token(token);
+    sc.set_extern_id(extern_id);
+    sc.set_email(email);
+    sc.set_name(String::from(name));
+    sc.set_provider(protocol::sessionsrv::OAuthProvider::GitHub);
+    sc
+}
+
+pub fn create_account(session: protocol::sessionsrv::SessionCreate) -> protocol::sessionsrv::Session {
+    let config = hab_sessionsrv::config::Config::default();
+    let data_store = hab_sessionsrv::data_store::DataStore::new(&config).unwrap();
+    let account_creation = data_store.find_or_create_account_via_session(&session, false, false, false);
+    let account = account_creation.unwrap();
+    account
+}
