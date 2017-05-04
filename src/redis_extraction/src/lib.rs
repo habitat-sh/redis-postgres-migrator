7e3a6343c16ac4cc22c99a05ba12772ab7d661a5
Nell Shamrell <nellshamrell@gmail.com>
2017-05-03 18:53:29 -0700

    working unwrapping client

--- src/redis_extraction/src/lib.rs
+++ src/redis_extraction/src/lib.rs
@@ -5,10 +5,7 @@ pub fn extract_accounts() {
    let client = redis_client();
 }
 
-fn redis_client() -> redis::RedisResult<()>{
-    let client = try!(redis::Client::open("redis://127.0.0.1/"));
-println!("{:?}", client);
-    let con = try!(client.get_connection());
-
-    Ok(())
+fn redis_client() -> redis::Client {
+    let client = redis::Client::open("redis://127.0.0.1/");
+    client.unwrap()
 }
