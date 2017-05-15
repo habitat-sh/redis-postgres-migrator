90a960a38a25bd3e2f463a042dcdd73ee0058405
Nell Shamrell <nellshamrell@gmail.com>
2017-05-15 15:46:29 -0700

    adds ability to call migrater to migrate origin keys from main method

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 extern crate redis_postgres_migrator as migrator;
+extern crate redis_lib;
 extern crate postgres_lib;
+extern crate redis_extraction;
+extern crate regex;
+use regex::Regex;
 
 use std::env;
 use migrator::migrators;
@@ -20,4 +24,23 @@ fn main() {
     migrators::package::PackageMigrator::new(redis_address.to_string(),
                                              originsrv_data_store.clone())
             .migrate();
+    migrate_origin_public_keys(redis_address);
+}
+
+fn migrate_origin_public_keys(redis_addr: &str) {
+    let redis_origins = redis_extraction::extract_origins(redis_addr);
+
+    let originsrv_data_store = postgres_lib::create_originsrv_data_store();
+
+		let re = Regex::new(r":(\d+)").unwrap();
+		for x in redis_origins {
+				for cap in re.captures_iter(&x) {
+				    let origin_id = &cap[1];
+						let redis_origin = redis_lib::find_origin_by_id(redis_addr, origin_id.parse::<u64>().unwrap());
+            let origin_keys = redis_lib::get_origin_keys_by_origin(redis_origin.get_name(), redis_addr);
+
+            migrators::origin_key::OriginKeyMigrator::new(redis_origin.get_name().to_string(), origin_keys, originsrv_data_store.clone());
+				}
+		}
+
 }
